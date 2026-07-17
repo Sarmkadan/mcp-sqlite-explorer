6bbbc1ebd45e187562681ae58201d68eb9332ff4
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tighten guard clauses in SqliteExplorerTestsJsonExtensions

--- tests/McpSqliteExplorer.Tests/SqliteExplorerTestsJsonExtensions.cs
+++ tests/McpSqliteExplorer.Tests/SqliteExplorerTestsJsonExtensions.cs
@@ -1,28 +1,43 @@
 using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace McpSqliteExplorer.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="SqliteExplorerTests"/>.
+/// </summary>
 public static class SqliteExplorerTestsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _camelCaseOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _camelCaseOptions = new(JsonSerializerDefaults.Web)
     {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
     };
 
+    /// <summary>
+    /// Serializes the specified <see cref="SqliteExplorerTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The value to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the value.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this SqliteExplorerTests value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        return indented
-            ? JsonSerializer.Serialize(value, _camelCaseOptions)
+        => indented
+            ? JsonSerializer.Serialize(value, new JsonSerializerOptions(_camelCaseOptions) { WriteIndented = true })
             : JsonSerializer.Serialize(value, _camelCaseOptions);
-    }
 
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="SqliteExplorerTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static SqliteExplorerTests? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
         try
         {
             return JsonSerializer.Deserialize<SqliteExplorerTests>(json, _camelCaseOptions);
@@ -33,9 +48,19 @@ public static class SqliteExplorerTestsJsonExtensions
         }
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="SqliteExplorerTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out SqliteExplorerTests? value)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
         try
         {
             value = JsonSerializer.Deserialize<SqliteExplorerTests>(json, _camelCaseOptions);
