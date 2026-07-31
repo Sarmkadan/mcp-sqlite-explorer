a7afbe9246530cd543a812ead0ab9f4c3e1479ce
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 17:37:15 +0300

    ISqliteConnectionFactory changes.

---
+++ ISqliteConnectionFactory.cs
@@ -0,0 +1,13 @@
+using System.Data.SQLite;
+using System.Data;
+
+namespace SqliteExplorer
+{
+    public class ISqliteConnectionFactory
+    {
+        public ISqliteConnectionFactory(string dbPath)
+        {
+Mode=ReadOnly
+        }
+    }
+}
